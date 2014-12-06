9b1e1f92a0af3a80fc2d3a503a84cf7d14efe0e9
lemuelcantos <lemuelinchrist@gmail.com>
2014-12-06 16:33:06 +0800

    Refactor - Extract all sheet music code into a new SheetMusic Class

--- app/src/main/java/com/lemuelinchrist/android/hymns/LyricContainer.java
+++ app/src/main/java/com/lemuelinchrist/android/hymns/LyricContainer.java
@@ -1,12 +1,9 @@
 package com.lemuelinchrist.android.hymns;
 
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
 import android.net.Uri;
-import android.os.Environment;
 import android.text.Html;
 import android.util.Log;
 import android.util.TypedValue;
@@ -20,8 +17,6 @@ import com.lemuelinchrist.android.hymns.entities.Stanza;
 import com.lemuelinchrist.android.hymns.utils.HymnTextFormatter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
@@ -41,6 +36,7 @@ public class LyricContainer extends LinearLayout {
     private SharedPreferences sharedPreferences;
     private HymnStack hymnStack;
     private LyricChangeListener lyricChangeListener;
+    private SheetMusic sheetMusic;
 
 
     public LyricContainer(Context context) {
@@ -66,6 +62,9 @@ public class LyricContainer extends LinearLayout {
 
         //set up hymn stack (for back button functionality)
         hymnStack = new HymnStack();
+
+        // initialize sheetMusic
+        sheetMusic = new SheetMusic(context);
 
 
     }
@@ -140,7 +139,7 @@ public class LyricContainer extends LinearLayout {
                     relatedConcat.append(r);
                 }
 
-                if(relatedConcat.length()>2)
+                if (relatedConcat.length() > 2)
                     text.append(relatedConcat.substring(2));
 
                 text.append("<br/>");
@@ -271,7 +270,7 @@ public class LyricContainer extends LinearLayout {
         } else if (size.equals("Extra Large")) {
             this.fontSize = 26;
 
-        }else if (size.equals("XXL")) {
+        } else if (size.equals("XXL")) {
             this.fontSize = 32;
         }
 
@@ -320,100 +319,6 @@ public class LyricContainer extends LinearLayout {
     }
 
 
-    public void getSheetMusic() {
-        // note: switch this value to "main" if you want to create a version that doesn't include sheet music svg's.
-        final String BRANCH = "guitar";
+    public void getSheetMusic() { sheetMusic.getSheetMusic(hymn);}
 
-        String sheetMusicLink = hymn.getSheetMusicLink();
-        if (sheetMusicLink != null) {
-            Intent i = new Intent(Intent.ACTION_VIEW);
-
-            if (BRANCH.equals("main")) {
-                String editedLink = sheetMusicLink.replace("_p.", "_g."); // p is for piano, g for guitar
-
-                i.setData(Uri.parse(editedLink));
-                context.startActivity(i);
-
-            } else if(BRANCH.equals("guitar")) {
-                InputStream in = null;
-                OutputStream out = null;
-                File file;
-
-                try {
-                    AssetManager assetManager = context.getAssets();
-                    String fileName;
-
-                    if (!hymn.hasOwnSheetMusic()) {
-                        fileName = hymn.getParentHymn() + ".svg";
-                    } else {
-                        fileName = hymn.getHymnId() + ".svg";
-                    }
-
-                    final File externalStorageSvgDir = new File(Environment.getExternalStorageDirectory(),"musicSheet");
-
-
-                    deleteDirectory(externalStorageSvgDir);
-                    if(!externalStorageSvgDir.mkdirs())
-                        Log.w(LyricContainer.class.getSimpleName(),"directory already exists. no need to create one.");
-
-                    file = new File(externalStorageSvgDir, fileName);
-                    in  = assetManager.open("guitarSvg/"+fileName);
-                    out = new FileOutputStream(file);
-
-
-                    byte[] buffer = new byte[1024];
-                    int read;
-                    while ((read = in.read(buffer)) != -1)
-                    {
-                        out.write(buffer, 0, read);
-                    }
-
-                    in.close();
-                    in = null;
-                    out.flush();
-                    out.close();
-                    out = null;
-
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(
-                            Uri.fromFile(file)
-                    );
-
-                    intent.setClassName("com.android.chrome", "com.google.android.apps.chrome.Main");
-
-                    context.startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(context, "Oops! Chrome not available! To display music sheet, please install Google Chrome from the Play Store.", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    Toast.makeText(context, "Sorry! Sheet music not available", Toast.LENGTH_SHORT).show();
-                    Log.e(LyricContainer.this.getClass().getSimpleName(), e.getMessage());
-                }
-
-
-            }
-
-
-        } else {
-            Toast.makeText(context, "Sorry! Sheet music not available", Toast.LENGTH_SHORT).show();
-
-        }
-    }
-
-    static public boolean deleteDirectory(File path) {
-        if( path.exists() ) {
-            File[] files = path.listFiles();
-            if (files == null) {
-                return true;
-            }
-            for(int i=0; i<files.length; i++) {
-                if(files[i].isDirectory()) {
-                    deleteDirectory(files[i]);
-                }
-                else {
-                    files[i].delete();
-                }
-            }
-        }
-        return( path.delete() );
-    }
 }
---
+++ app/src/main/java/com/lemuelinchrist/android/hymns/SheetMusic.java
@@ -0,0 +1,128 @@
+package com.lemuelinchrist.android.hymns;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.lemuelinchrist.android.hymns.entities.Hymn;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by lemuelcantos on 6/12/14.
+ */
+public class SheetMusic {
+    private Context context;
+
+    public SheetMusic(Context context) {
+        this.context = context;
+
+    }
+
+    public void getSheetMusic(Hymn hymn ) {
+        // note: switch this value to "main" if you want to create a version that doesn't include sheet music svg's.
+        final String BRANCH = "guitar";
+
+        String sheetMusicLink = hymn.getSheetMusicLink();
+        if (sheetMusicLink != null) {
+            Intent i = new Intent(Intent.ACTION_VIEW);
+
+            if (BRANCH.equals("main")) {
+                String editedLink = sheetMusicLink.replace("_p.", "_g."); // p is for piano, g for guitar
+
+                i.setData(Uri.parse(editedLink));
+                context.startActivity(i);
+
+            } else if (BRANCH.equals("guitar")) {
+
+                String fileName;
+
+                if (!hymn.hasOwnSheetMusic()) {
+                    fileName = hymn.getParentHymn() + ".svg";
+                } else {
+                    fileName = hymn.getHymnId() + ".svg";
+                }
+
+                generateGuitarSheet(fileName);
+
+            }
+
+        } else {
+            Toast.makeText(context, "Sorry! Sheet music not available", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            if (files == null) {
+                return true;
+            }
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
+    }
+
+    void generateGuitarSheet(String fileName) {
+        try {
+
+            AssetManager assetManager = context.getAssets();
+            File file;
+            InputStream in;
+            OutputStream out;
+            final File externalStorageSvgDir = new File(Environment.getExternalStorageDirectory(), "musicSheet");
+
+
+            deleteDirectory(externalStorageSvgDir);
+            if (!externalStorageSvgDir.mkdirs())
+                Log.w(LyricContainer.class.getSimpleName(), "directory already exists. no need to create one.");
+
+            file = new File(externalStorageSvgDir, fileName);
+            in = assetManager.open("guitarSvg/" + fileName);
+            out = new FileOutputStream(file);
+
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(
+                    Uri.fromFile(file)
+            );
+
+            intent.setClassName("com.android.chrome", "com.google.android.apps.chrome.Main");
+
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(context, "Oops! Chrome not available! To display music sheet, please install Google Chrome from the Play Store.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(context, "Sorry! Sheet music not available", Toast.LENGTH_SHORT).show();
+            Log.e(SheetMusic.class.getSimpleName(), e.getMessage());
+        }
+
+    }
+}
