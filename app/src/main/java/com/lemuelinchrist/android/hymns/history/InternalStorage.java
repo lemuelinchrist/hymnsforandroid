c725b7b566e67e8441257f8e72dde5b8f9b0a242
lemuelcantos <lemuelinchrist@gmail.com>
2014-12-07 18:14:04 +0800

    Create History Mechanism

---
+++ app/src/main/java/com/lemuelinchrist/android/hymns/history/InternalStorage.java
@@ -0,0 +1,33 @@
+package com.lemuelinchrist.android.hymns.history;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by lemuelcantos on 7/12/14.
+ */
+public final class InternalStorage{
+
+    private InternalStorage() {}
+
+    public static void writeObject(Context context, String key, Object object) throws IOException {
+        FileOutputStream fos = context.openFileOutput(key, Context.MODE_PRIVATE);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(object);
+        oos.close();
+        fos.close();
+    }
+
+    public static Object readObject(Context context, String key) throws IOException,
+            ClassNotFoundException {
+        FileInputStream fis = context.openFileInput(key);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Object object = ois.readObject();
+        return object;
+    }
+}
