e49317a4d55d72b4a59364b6613da1ce7ea8ebd6
lemuelcantos <lemuelinchrist@gmail.com>
2015-02-17 00:17:19 +0800

    minor tweaks. RC 1

---
+++ app/src/main/java/com/lemuelinchrist/android/hymns/MusicPlayerListener.java
@@ -0,0 +1,9 @@
+package com.lemuelinchrist.android.hymns;
+
+/**
+ * Created by lemuelcantos on 15/2/15.
+ */
+public interface MusicPlayerListener {
+    public void onMusicStarted();
+    public void onMusicStopped();
+}
