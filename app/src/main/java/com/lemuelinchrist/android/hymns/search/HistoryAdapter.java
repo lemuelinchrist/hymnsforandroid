6e44f39e5df34d6b52414342ededd9a726a8c30e
lemuelcantos <lemuelinchrist@gmail.com>
2015-10-31 15:14:43 +0800

    Refactor Search code. remove duplicates to prepare for search capability expansion

--- app/src/main/java/com/lemuelinchrist/android/hymns/search/HistoryAdapter.java
+++ app/src/main/java/com/lemuelinchrist/android/hymns/search/HistoryAdapter.java
@@ -18,51 +18,22 @@ import com.lemuelinchrist.android.hymns.history.HistoryRecord;
 /**
  * Created by lemuelcantos on 27/1/15.
  */
-public class HistoryAdapter extends RecyclerView.Adapter<IndexViewHolder> {
+public class HistoryAdapter extends SearchAdapter {
 
-    private final Context context;
-    private final int layout;
     private final HistoryRecord[] historyLogBookList;
 
     public HistoryAdapter(Context context, int layout) {
-        this.context=context;
-        this.layout=layout;
+        super(context, layout);
         historyLogBookList = new HistoryLogBook(context).getOrderedRecordList();
 
-
-
     }
 
     @Override
-    public IndexViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(layout, viewGroup, false);
-
-
-        IndexViewHolder vh = new IndexViewHolder(rowView);
-        return vh;
-    }
-
-    @Override
-    public void onBindViewHolder(final IndexViewHolder holder, int position) {
+    public void provisionHolder(final IndexViewHolder holder, int position) {
         HistoryRecord record = historyLogBookList[position];
         holder.list_item.setText(record.getFirstLine());
         holder.imageView.setImageResource(context.getResources().getIdentifier(record.getHymnGroup().toLowerCase(), "drawable", context.getPackageName()));
         holder.hymnNo=record.getHymnId();
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent data = new Intent();
-                data.setData(Uri.parse(holder.hymnNo));
-                Activity currentActivity = (Activity) context;
-                currentActivity.setResult(currentActivity.RESULT_OK, data);
-                currentActivity.finish();
-            }
-        });
-
     }
 
     @Override
