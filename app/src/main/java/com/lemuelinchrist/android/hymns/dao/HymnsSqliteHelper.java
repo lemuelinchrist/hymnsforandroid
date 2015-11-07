d49a041c98bddc961fa1f641cf8eff8767d7edab
lemuelcantos <lemuelinchrist@gmail.com>
2015-11-07 21:43:09 +0800

    migrated long beach hymns

--- app/src/main/java/com/lemuelinchrist/android/hymns/dao/HymnsSqliteHelper.java
+++ app/src/main/java/com/lemuelinchrist/android/hymns/dao/HymnsSqliteHelper.java
@@ -18,7 +18,7 @@ import java.io.OutputStream;
 public class HymnsSqliteHelper extends SQLiteOpenHelper {
 
 
-    private static final int VERSION = 65;
+    private static final int VERSION = 68;
     private static final String DATABASE_NAME = "hymns.sqlite";
     private static File DATABASE_FILE;
 
