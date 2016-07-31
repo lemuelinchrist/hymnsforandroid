a357ec6a4e00baddc6cf1dca56aad6bc212f6059
lemuelcantos <lemuelinchrist@gmail.com>
2016-07-31 16:27:09 +0800

    fix spanish single space rule

--- databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionSpanish.groovy
+++ databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionSpanish.groovy
@@ -40,6 +40,8 @@ class ProvisionSpanish {
                     if (line.isEmpty()) throw new RuntimeException("blank line after chorus");
 //                    if (line.isEmpty()) println("*****${hymnNumber}");
 
+                } else if(!line[0].isInteger()) {
+                    println "non int line: " +line
                 }
 
             }
