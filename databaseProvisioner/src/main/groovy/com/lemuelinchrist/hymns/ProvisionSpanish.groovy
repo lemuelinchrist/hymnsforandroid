1899ca6a0204ee630185b38bd86a72c861c0765a
lemuelcantos <lemuelinchrist@gmail.com>
2016-07-31 19:56:59 +0800

    Create HymnsEntity at beginning of two blank lines. Extract Related hymns for spanish

--- databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionSpanish.groovy
+++ databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionSpanish.groovy
@@ -2,43 +2,64 @@ package com.lemuelinchrist.hymns
 
 import com.lemuelinchrist.hymns.lib.Dao
 import com.lemuelinchrist.hymns.lib.beans.HymnsEntity
+import com.lemuelinchrist.hymns.lib.beans.StanzaEntity
 
 /**
  * Created by lcantos on 29/7/2016.
  */
 class ProvisionSpanish {
+    static File spanishFile;
+    static File spanishRelatedFile;
 
     public static void main(String[] args) throws Exception {
-//        Dao dao = new Dao();
-//        HymnsEntity hymn;
-        File file = new File(this.getClass().getResource("/spanish.txt").getPath());
+        Dao dao = new Dao();
+        spanishFile = new File(this.getClass().getResource("/spanish.txt").getPath());
+        spanishRelatedFile = new File(this.getClass().getResource("/spanishRelated.txt").getPath());
 
-//        for(String line: file.readLines()) {
-//            println line;
-//        }
-        Iterator<String> iterator = file.iterator();
+        Iterator<String> iterator = spanishFile.iterator();
+        Iterator<String> relatedIterator = spanishRelatedFile.iterator();
         Integer hymnNumber = 0;
+        Integer stanzaCounter = 0;
+        HymnsEntity hymn=null;
+        StanzaEntity stanza=null;
         while (iterator.hasNext()) {
             String line = iterator.next().trim();
             if(line.isEmpty()) {
                 line = iterator.next().trim();
                 if (line.isEmpty()) {
+                    // finalize current hymn before iterating
+                    println hymn;
+
                     hymnNumber++;
                     if (hymnNumber==501) break;
-//                    println("searching for ${hymnNumber}")
+
                     line = iterator.next().trim();
                     if(!line.equals(hymnNumber+".")) {
                         throw new RuntimeException("Missing Hymn: " + hymnNumber);
                     }
 
+                    println "Generating Spanish Hymn ${hymnNumber}..."
+                    hymn=new HymnsEntity();
+                    hymn.id='S'+hymnNumber
+                    hymn.no=hymnNumber
+                    hymn.hymnGroup='S'
 
-                    println "Hymn ${hymnNumber} found!"
+                    def related = relatedIterator.next().trim();
+                    if (related.isEmpty()) {
+                        hymn.parentHymn=null
+                    } else if (!related[0].equals("C")) {
+                        // english
+                        hymn.parentHymn="E"+related
+                    } else {
+                        // chinese
+                        hymn.parentHymn = related;
+                    }
+
 
 
                 } else if (line.toLowerCase().contains("coro:")) {
                     line = iterator.next().trim();
                     if (line.isEmpty()) throw new RuntimeException("blank line after chorus");
-//                    if (line.isEmpty()) println("*****${hymnNumber}");
 
                 } else if(!line[0].isInteger() && !line[0].equals('(') && !line[0].equals('+')) {
                     throw new RuntimeException("Invalid start of stanza");
@@ -49,4 +70,5 @@ class ProvisionSpanish {
         }
 
     }
+
 }
