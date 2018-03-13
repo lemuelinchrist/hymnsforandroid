1dbd53a48b1ad5724236f3a9944741a19860e5e3
lemuel <lemuelinchrist@gmail.com>
2018-03-13 23:44:08 +0800

    prepare to create Lyrics searcher.

---
+++ app/src/main/java/com/lemuelinchrist/android/hymns/search/LyricsTabFragment.java
@@ -0,0 +1,55 @@
+package com.lemuelinchrist.android.hymns.search;
+
+import com.lemuelinchrist.android.hymns.R;
+import com.lemuelinchrist.android.hymns.dao.HymnsDao;
+import com.lemuelinchrist.android.hymns.search.searchadapters.AuthorAdapter;
+
+/**
+ * @author Lemuel Cantos
+ * @since 13/3/2018
+ */
+public class LyricsTabFragment extends TabFragment {
+    private HymnsDao dao;
+
+    @Override
+    public int getSearchTabIndex() {
+        return 4;
+    }
+
+    @Override
+    public String getTabName() {
+        return "All Lyrics";
+    }
+
+    @Override
+    public void setRecyclerViewAdapter() {
+        dao = new HymnsDao(container.getContext());
+        dao.open();
+
+        mRecyclerView.setAdapter(new AuthorAdapter(container.getContext(),
+                dao.getAuthorsList(""), R.layout.recyclerview_hymn_list));
+
+    }
+
+    @Override
+    public void cleanUp() {
+        dao.close();
+
+    }
+
+    @Override
+    public boolean canBeSearched() {
+        return true;
+    }
+
+    @Override
+    public void setSearchFilter(String filter) {
+        mRecyclerView.setAdapter(new AuthorAdapter(container.getContext(),
+                dao.getAuthorsList(filter)
+                , R.layout.recyclerview_hymn_list));    }
+
+    @Override
+    public int getIcon() {
+        return R.drawable.ic_receipt_grey;
+    }
+}
