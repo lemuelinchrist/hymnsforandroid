d4600d4c4158b85420571fd24d29bc1ce873ad41
lemuel <lemuelinchrist@gmail.com>
2018-03-17 01:48:41 +0800

    change action bar color according to night mode / day mode

--- app/src/main/java/com/lemuelinchrist/android/hymns/style/Theme.java
+++ app/src/main/java/com/lemuelinchrist/android/hymns/style/Theme.java
@@ -1,5 +1,8 @@
 package com.lemuelinchrist.android.hymns.style;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import com.lemuelinchrist.android.hymns.HymnGroup;
 import com.lemuelinchrist.android.hymns.R;
 
 import java.util.ArrayList;
@@ -7,15 +10,26 @@ import java.util.List;
 
 public enum Theme {
 
-    DARK(R.layout.lyric_container_night), LIGHT(R.layout.lyric_container);
+    DARK(R.layout.lyric_container_night, Color.parseColor("#000000")), LIGHT(R.layout.lyric_container, null);
+
+    int style;
+    private final Integer actionBarColor;
+
+
+    Theme(Integer style, Integer actionBarColor) {
+        this.style=style;
+        this.actionBarColor=actionBarColor;
+    }
 
     public int getStyle() {
         return style;
     }
-
-    int style;
-
-    Theme(int style) {
-        this.style=style;
+    public ColorDrawable getActionBarColor(HymnGroup hymnGroup) {
+        if (actionBarColor==null) {
+            return new ColorDrawable(hymnGroup.getDayColor());
+        }
+        else {
+            return new ColorDrawable(actionBarColor);
+        }
     }
 }
