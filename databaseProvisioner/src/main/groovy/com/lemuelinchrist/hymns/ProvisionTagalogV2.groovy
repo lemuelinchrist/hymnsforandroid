83c581851b467480f694d25464fdc49fee0d1b78
lemuel <lemuelinchrist@gmail.com>
2018-07-07 16:11:55 +0800

    create code to print all lyrics

--- databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionTagalogV2.groovy
+++ databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionTagalogV2.groovy
@@ -1,7 +1,10 @@
 package com.lemuelinchrist.hymns
 
+import com.lemuelinchrist.hymns.lib.beans.HymnsEntity
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements;
 
 import java.io.File;
 
@@ -10,23 +13,43 @@ import java.io.File;
  * @since 1/7/2018
  */
 class ProvisionTagalogV2 {
-    public static String ROOT_PATH="tagalogV2/";
-    Document doc
-    int hymnNo
+
 
     ProvisionTagalogV2() {
-        doc=getDocumentFromHymn(1,"")
-        doc.select("#t001")
+
+
         for(int x=0;x<100;x++) {
 
         }
 
     }
 
-    static void main(String[] args) {
-        def tagalog = new ProvisionTagalogV2();
-        println tagalog.getfileFromHymn(233).exists();
 
+    static void main(String[] args) {
+        println new HymnElement(1)
+
+    }
+
+
+}
+
+class HymnElement {
+    public static String ROOT_PATH="tagalogV2/";
+
+    Document doc
+    int hymnNo
+    String htmlId;
+    def baseElement
+
+    public HymnElement(int no, String prefix="") {
+        this.doc=getDocumentFromHymn(no,prefix)
+        htmlId="#t" + String.format("%03d", no)
+        baseElement=doc.select(htmlId)[0]
+
+    }
+
+    public String getLyrics() {
+        baseElement.parent().nextElementSibling().select(".hymnbody p")[0].html()
     }
 
     private Document getDocumentFromHymn(int no, String prefix="") {
@@ -39,4 +62,9 @@ class ProvisionTagalogV2 {
         println "getting file: " + path
         return new File(this.getClass().getClassLoader().getResource(path).getPath())
     }
+
+    String toString() {
+        getLyrics()
+    }
+
 }
