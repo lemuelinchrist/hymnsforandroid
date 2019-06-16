fd55243b71b676140c5daf8f211912f1884c05ea
lemuel <lemuelinchrist@gmail.com>
2019-06-16 15:50:34 +0800

    split text by hymn

---
+++ databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionJapanese.java
@@ -0,0 +1,83 @@
+package com.lemuelinchrist.hymns;
+
+import com.lemuelinchrist.hymns.lib.Dao;
+import com.lemuelinchrist.hymns.lib.beans.HymnsEntity;
+import com.lemuelinchrist.hymns.lib.beans.StanzaEntity;
+import groovy.util.logging.Slf4j;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Lemuel Cantos
+ * @since 15/6/2019
+ */
+@Slf4j
+public class ProvisionJapanese {
+    File japaneseFile;
+    File japaneseSupplementFile;
+
+    public static void main(String[] args) throws Exception {
+        ProvisionJapanese japanese = new ProvisionJapanese();
+        japanese.provision();
+    }
+
+    public void provision() throws Exception {
+        Dao dao = new Dao();
+        japaneseFile = new File(this.getClass().getResource("/japanese.txt").getPath());
+        StringBuilder logString = new StringBuilder();
+        japaneseSupplementFile = new File(this.getClass().getResource("/japaneseSupplement.txt").getPath());
+
+        List<String> lines = Files.lines(japaneseFile.toPath()).collect(Collectors.toList());
+        Integer hymnNumber = 0;
+        Integer stanzaCounter = 0;
+        Integer stanzaOrderCounter = 0;
+        HymnsEntity hymn = null;
+        StanzaEntity stanza = null;
+        StringBuilder stanzaBuilder = null;
+        PrintStream out = new PrintStream(System.out, true, "UTF-8");
+
+
+        for(int x = 0; x<23349; x++) {
+            String line = lines.get(x).trim();
+
+            if (line.isEmpty() &&
+                    (x+1)!=lines.size() &&
+                    lines.get(x+1).trim().isEmpty()) {
+                while((x+1)!=lines.size() &&
+                        lines.get(++x).trim().isEmpty());
+                logString.append(++hymnNumber + " ");
+                logString.append(lines.get(x+1).trim());
+                logString.append("\n");
+
+                continue;
+            }
+        }
+
+//        stream.forEach(a -> {
+//            System.out.println(a);
+//            logString.append(a);
+//        });
+
+
+//        while (iterator.hasNext()) {
+//
+//            String line = iterator.next().trim();
+//            out.println line
+//        }
+        log(logString.toString());
+    }
+
+    public void log(String text) {
+        try {
+            Files.write(Paths.get("TEST.txt"), text.getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
