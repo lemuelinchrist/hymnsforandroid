e1b5d5667b3a8a32483c2a0a6bc598e0686b4a08
Lemuel Cantos <lecantos@paypal.com>
2019-06-18 10:54:43 +0800

    fix chorus

--- databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionJapanese.java
+++ databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionJapanese.java
@@ -27,16 +27,21 @@ public class ProvisionJapanese {
     private int x = 0;
     private String line;
     private List<String> lines;
+    private static StringBuilder logString = new StringBuilder();
+    private StringBuilder errors = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         ProvisionJapanese japanese = new ProvisionJapanese();
-        japanese.provision();
+        try {
+            japanese.provision();
+        } finally {
+            log(logString.toString());
+        }
     }
 
     public void provision() throws Exception {
         Dao dao = new Dao();
         japaneseFile = new File(this.getClass().getResource("/japanese.txt").getPath());
-        StringBuilder logString = new StringBuilder();
         japaneseSupplementFile = new File(this.getClass().getResource("/japaneseSupplement.txt").getPath());
 
         lines = Files.lines(japaneseFile.toPath()).collect(Collectors.toList());
@@ -50,79 +55,77 @@ public class ProvisionJapanese {
         for (x = 0; hymnNumber != 780; x++) {
             line = lines.get(x).trim();
 
-            if (isStartOfHymn()) {
-                while ((x + 1) != lines.size() &&
-                        lines.get(++x).trim().isEmpty()) {
-                    ;
-                }
+            while ((x + 1) != lines.size() &&
+                    lines.get(++x).trim().isEmpty()) {
 
-                String relatedLine = lines.get(x + 1).trim();
-                logString.append(++hymnNumber + " ");
-                System.out.println("processing Hymn#" + hymnNumber);
-                logString.append(relatedLine);
-                logString.append("\n");
-
-                Integer englishRelated = null;
-                if (relatedLine.contains("英")) {
-                    englishRelated = Integer.parseInt(relatedLine
-                            .substring(relatedLine.indexOf("英") + 1).replaceAll("[^\\d]", ""));
-                }
-
-                String categoryLine = lines.get(x).trim();
-                assert categoryLine.contains("―");
-
-                hymn = new HymnsEntity();
-                hymn.setId("J" + hymnNumber);
-                hymn.setHymnGroup("J");
-                hymn.setNo(hymnNumber.toString());
-                hymn.setMainCategory(categoryLine.split("―")[0].trim());
-                hymn.setSubCategory(categoryLine.split("―")[1].trim());
-                if (englishRelated != null) {
-                    hymn.setParentHymn("E" + englishRelated);
-                }
-
-                logString.append(hymn.toString() + "\n");
-
-                x++;
-                x++;
-                line = convertJapCharacters(x);
-                if (!line.equals("1") && !line.equals("note")) {
-                    throw new RuntimeException("line "
-                            + "after hymn "
-                            + "number "
-                            + "should "
-                            + "be first "
-                            + "stanza but "
-                            + "is: " + line);
-                }
-
-                stanzaCounter = 0;
-                while (true) {
-                    if (!line.equals("note") && !line.equals("(復)")) {
-                        stanzaCounter++;
-                        System.out.println("stanza " + stanzaCounter);
-                        if (Integer.parseInt(line) != stanzaCounter) {
-                            throw new RuntimeException(line + "!= "
-                                    + "stnazacounter: " + stanzaCounter);
-                        }
-                    }
-                    while (!line.isEmpty()) {
-                        line = lines.get(++x).trim();
-                    }
-                    if (isStartOfHymn()) {
-                        break;
-                    }
-                    line = convertJapCharacters(++x);
-
-                }
-
-                //                while(!isStartOfHymn()) {
-                //                    x++;
-                //
-                //                }
-
-                continue;
             }
+
+            String relatedLine = lines.get(x + 1).trim();
+            logString.append(++hymnNumber + " ");
+            System.out.println("processing Hymn#" + hymnNumber);
+            logString.append(relatedLine);
+            logString.append("\n");
+
+            Integer englishRelated = null;
+            if (relatedLine.contains("英")) {
+                englishRelated = Integer.parseInt(relatedLine
+                        .substring(relatedLine.indexOf("英") + 1).replaceAll("[^\\d]", ""));
+            }
+
+            String categoryLine = lines.get(x).trim();
+            assert categoryLine.contains("―");
+
+            hymn = new HymnsEntity();
+            hymn.setId("J" + hymnNumber);
+            hymn.setHymnGroup("J");
+            hymn.setNo(hymnNumber.toString());
+            hymn.setMainCategory(categoryLine.split("―")[0].trim());
+            hymn.setSubCategory(categoryLine.split("―")[1].trim());
+            if (englishRelated != null) {
+                hymn.setParentHymn("E" + englishRelated);
+            }
+
+            logString.append(hymn.toString() + "\n");
+
+            x++;
+            x++;
+            line = convertJapCharacters(x);
+            if (!line.equals("1") && !line.equals("note")) {
+                throw new RuntimeException("line "
+                        + "after hymn "
+                        + "number "
+                        + "should "
+                        + "be first "
+                        + "stanza but "
+                        + "is: " + line);
+            }
+
+            stanzaCounter = 0;
+            while (true) {
+                if (!line.equals("note") && !line.equals("(復)")) {
+                    stanzaCounter++;
+                    System.out.println("stanza " + stanzaCounter);
+                    if (Integer.parseInt(line) != stanzaCounter) {
+                        throw new RuntimeException(line + "!= "
+                                + "stnazacounter: " + stanzaCounter);
+                    }
+                }
+                while (!line.isEmpty()) {
+                    line = lines.get(++x).trim();
+                }
+                if (isStartOfHymn()) {
+                    break;
+                }
+                line = convertJapCharacters(++x);
+
+            }
+
+            //                while(!isStartOfHymn()) {
+            //                    x++;
+            //
+            //                }
+
+            continue;
         }
 
         log(logString.toString());
@@ -151,7 +154,7 @@ public class ProvisionJapanese {
                 lines.get(x + 1).trim().isEmpty();
     }
 
-    public void log(String text) {
+    public static void log(String text) {
         try {
             Files.write(Paths.get("TEST.txt"), text.getBytes());
         } catch (Exception e) {
