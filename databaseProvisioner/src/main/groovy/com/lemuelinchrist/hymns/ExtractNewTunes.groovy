9e1722688357975f4c721f78ca61d700af62b2a7
lemuel <lemuelinchrist@gmail.com>
2020-02-19 21:55:26 +0800

    add New Tunes to BF

--- databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ExtractNewTunes.groovy
+++ databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ExtractNewTunes.groovy
@@ -19,5 +19,112 @@ class ExtractNewTunes {
             HymnsEntity hymn = HymnalNetExtractor.convertWebPageToHymn(Constants.NEW_TUNES_SHEET_LINK, "" + x, "BF", "" + bfNo++)
             dao.save(hymn);
         }
+
+        newHymnNo = [ 284,350,394,711,720]
+        bfNo =450;
+        for (int x: newHymnNo){
+            HymnsEntity hymn = HymnalNetExtractor.convertWebPageToHymn(Constants.NEW_TUNES_SHEET_LINK, "" + x + "b", "BF", "" + bfNo++)
+            dao.save(hymn);
+        }
     }
 }
+
+/** list of all New Tunes:
+ 12
+ 33
+ 96
+ 154
+ 165
+ 172
+ 204
+ 208
+ 223
+ 251
+ 252
+ 284
+ 285
+ 287
+ 293
+ 321
+ 323
+ 325
+ 339
+ 343
+ 350
+ 352
+ 356
+ 367
+ 369
+ 374
+ 377
+ 378
+ 383
+ 389
+ 394
+ 407
+ 412
+ 426
+ 431
+ 432
+ 434
+ 437
+ 439
+ 449
+ 474
+ 477
+ 496
+ 498
+ 512
+ 513
+ 521
+ 523
+ 525
+ 531
+ 543
+ 547
+ 553
+ 575
+ 578
+ 599
+ 600
+ 605
+ 641
+ 642
+ 643
+ 647
+ 673
+ 708
+ 711
+ 716
+ 720
+ 721
+ 723
+ 724
+ 731
+ 788
+ 789
+ 806
+ 857
+ 995
+ 1008
+ 1040
+ 1048
+ 1049
+ 1050
+ 1057
+ 1079
+ 1158
+ 1174
+ 1210
+ 1222
+ 1238
+ 1271
+ 1278
+ 1307
+ 1325
+ 284b
+ 350b
+ 394b
+ 711b
+ 720b
+**/
