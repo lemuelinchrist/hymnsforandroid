9a150a80109af95b8d5156aee33d9fd4c908b09f
Coder Basket <amazingview4@gmail.com>
2023-05-18 19:12:36 +0800

    add gitignore

---
+++ app/build/generated/source/buildConfig/debug/com/lemuelinchrist/android/hymns/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lemuelinchrist.android.hymns;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lemuelinchrist.android.hymns";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 178;
+  public static final String VERSION_NAME = "v4.6-Guitar";
+}
