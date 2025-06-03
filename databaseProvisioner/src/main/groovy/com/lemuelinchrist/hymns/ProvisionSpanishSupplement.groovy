f89faa95a84642ae4ff30ba70700ec8c164470d6
lemuelinchrist <lemuelinchrist@gmail.com>
2025-06-03 10:02:06 +0800

    spanish supplement-3nd June, update SQLite data rows, and adjust metadata for consistency.

--- databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionSpanishSupplement.groovy
+++ databaseProvisioner/src/main/groovy/com/lemuelinchrist/hymns/ProvisionSpanishSupplement.groovy
@@ -35,7 +35,7 @@ class ProvisionSpanishSupplement {
     }
 
     void removeSpanishHymns() {
-        for(int x=2001;x<=2371;x++) {
+        for(int x=2001;x<=2373;x++) {
             dao.delete("S"+x)
         }
     }
